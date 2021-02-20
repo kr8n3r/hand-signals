091fbe7485d2ec5ab3b28ad9d8c1f10269eceee9
kr8n3r <jani@kraner.dev>
2021-02-20 00:43:57 +0000

    Initial

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <ReactionTray />
+</template>
+
+<script setup>
+import ReactionTray from './components/ReactionTray.vue'
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+body {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background-color: #2c3e50;
+  margin:0;
+  padding:0;
+}
+</style>
---
+++ src/components/ReactionTray.vue
@@ -0,0 +1,66 @@
+<template>
+<div class="reaction-tray">
+  <button @click="toggleReactions()">Mimise</button>
+  <div class="reaction-tray__reactions"
+    :class="{ active: isVisible }">
+    <Reaction reaction='Handup' />
+    <Reaction reaction='Agree' />
+    <Reaction reaction='Disagree' />
+    <Reaction reaction='Respond' />
+    <Reaction reaction='Clarify' />
+    <Reaction reaction='Point of Order' />
+    <Reaction reaction='Block' />
+    <Reaction reaction='Oppose' />
+  </div>
+</div>
+</template>
+
+<script>
+import Reaction from './Reaction.vue'
+
+export default {
+  components: {
+    Reaction
+  },
+  props: {
+    reaction: String
+  },
+  data: function () {
+    return {
+      isVisible: false
+    }
+  },
+   methods: {
+    toggleReactions: function() {
+      this.isVisible = !this.isVisible;
+    }
+  }
+}
+
+
+</script>
+
+<style scoped>
+.reaction-tray {
+  position: absolute;
+  top:0;
+  left: 0;
+}
+.reaction-tray__reactions {
+  background: white;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 0.5em;
+  transform: translateX(-200px);
+  border-radius: 0 0 8px;
+  transition-duration: 0.25s;
+  transition-property: transform;
+  transition-timing-function: cubic-bezier(0.4, 0, 1, 1);
+}
+
+.active {
+  transform: translateX(0);
+}
+
+
+</style>
